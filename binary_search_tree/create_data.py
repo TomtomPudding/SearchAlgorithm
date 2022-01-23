8d0f6872618047cce66f90ef97e4f1c00ae777ff
TomtomPudding <tomtomhobby1125@gmail.com>
2022-01-23 22:49:26 +0900

    [update] measure data

--- binary_search_tree/create_data.py
+++ binary_search_tree/create_data.py
@@ -1,7 +1,7 @@
 import random
 
 if __name__ == '__main__':
-    base_array = list(range(1000000))
-    random_array = map(str, random.sample(base_array, 100000))
-    with open('data_100000.txt', mode='w') as f:
+    base_array = list(range(10000000))
+    random_array = map(str, random.sample(base_array, 1000000))
+    with open('data_1000000.txt', mode='w') as f:
         f.write('\n'.join(random_array))
